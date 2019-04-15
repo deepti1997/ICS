c797f2d2f810956f98d7e860a0e8ece8fde26464
deepti1997 <deepti.tanpure@gmail.com>
2019-04-16 02:40:32 +0530

    Add files via upload

---
+++ AES.java
@@ -0,0 +1,87 @@
+package AES;
+
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES
+{
+    static String plainText = "This is a plain text which need to be encrypted by AES Algorithm with CBC Mode";
+    
+    public static void main(String[] args) throws Exception
+    {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+
+        // Generate Key
+        SecretKey key = keyGenerator.generateKey();
+
+        // Generating IV.
+        byte[] IV = new byte[16];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(IV);
+        
+        System.out.println("Original Text  : "+plainText);
+        
+        byte[] cipherText = encrypt(plainText.getBytes(),key, IV);
+        System.out.println("Encrypted Text : "+Base64.getEncoder().encodeToString(cipherText) );
+        
+        String decryptedText = decrypt(cipherText,key, IV);
+        System.out.println("DeCrypted Text : "+decryptedText);
+        
+    }
+    
+    public static byte[] encrypt (byte[] plaintext,SecretKey key,byte[] IV ) throws Exception
+    {
+        //Get Cipher Instance
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        
+        //Create SecretKeySpec
+        SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
+        
+        //Create IvParameterSpec
+        IvParameterSpec ivSpec = new IvParameterSpec(IV);
+        
+        //Initialize Cipher for ENCRYPT_MODE
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        
+        //Perform Encryption
+        byte[] cipherText = cipher.doFinal(plaintext);
+        
+        return cipherText;
+    }
+    
+    public static String decrypt (byte[] cipherText, SecretKey key,byte[] IV) throws Exception
+    {
+        //Get Cipher Instance
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        
+        //Create SecretKeySpec
+        SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
+        
+        //Create IvParameterSpec
+        IvParameterSpec ivSpec = new IvParameterSpec(IV);
+        
+        //Initialize Cipher for DECRYPT_MODE
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        
+        //Perform Decryption
+        byte[] decryptedText = cipher.doFinal(cipherText);
+        
+        return new String(decryptedText);
+    }
+}
+
+/********
+
+Original Text  : This is a plain text which need to be encrypted by AES Algorithm with CBC Mode
+Encrypted Text : etkRnI5ca6bkOW9VlFSJ0edZEnNT65BvzFRExJqBtZdlbPbeYYKJron/c60l3zH+8rNEFPOQDOtGJqg3nu6K+DXLsS5FuaicxlgBx4LxApA=
+DeCrypted Text : This is a plain text which need to be encrypted by AES Algorithm with CBC Mode
+
+********/
